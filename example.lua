5261f33a970d62a32967f4c404c88f421c462c5a
boop71 <77131003+boop71@users.noreply.github.com>
2021-01-25 22:15:38 +0100

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,1 +1,22 @@
-
+local Admin = loadstring(game:HttpGet("https://pastebin.com/raw/Mwj492sT"))()
+local Admin2 = Admin.load(getgenv().themes.dark, "Project name here", true) 
+Admin2.addCommand({name = "testCommand",desc = "this is a command!",callback = function(v)
+    local m = Instance.new("Message",workspace)
+    m.Text = v
+    wait(1)
+    m:Destroy()
+end})
+Admin2.addCommand({name = "goto",desc = "teleports you to the target!",callback = function(v,b)
+    local t = Admin2.getplayers(v)
+    for i,v2 in pairs(t) do
+        pcall(function()
+            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v2.Character.HumanoidRootPart.Position+Vector3.new(0,0,3))
+        end)
+    end
+    if b then
+        local m = Instance.new("Message",workspace)
+        m.Text = "Cheese!"
+        wait(1)
+        m:Destroy()
+    end
+end})
